0e4eeebc530bfd80721e8dc37628fe11df4487de
Theme222 <sira.tongsima@yahoo.com>
2026-04-13 21:16:12 +0700

    added extra campground info to getReview which is there for some reason

--- src/libs/reviews.ts
+++ src/libs/reviews.ts
@@ -1,24 +1,27 @@
-import { fetchWrapper } from "./funcs"
-import { APIResponseMultiple, APIResponseSingle, Review } from "./types"
+import { fetchWrapper } from "./funcs";
+import { APIResponseMultiple, APIResponseSingle, Review } from "./types";
 
 const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL || process.env.BACKEND_URL;
 
 export async function getCampgroundReviews(campgroundId: string) {
   return fetchWrapper<APIResponseMultiple<Review>>(
-    `${backendUrl}/api/v1/campgrounds/${campgroundId}/reviews`, {
-      method: "GET",
-    }
-  )
+    `${backendUrl}/api/v1/campgrounds/${campgroundId}/reviews`,
+    { method: "GET", },
+  );
+}
+
+interface AdditionalCampground {
+  _id: string;
+  name: string;
+  province: string;
 }
 
 export async function getReview(id: string) {
-  return fetchWrapper<APIResponseSingle<Review>>(
-    `${backendUrl}/api/v1/reviews/${id}`, {
-      method: "GET",
-    }
-  )
+  return fetchWrapper<APIResponseSingle<Review & AdditionalCampground>>(
+    `${backendUrl}/api/v1/reviews/${id}`,
+    { method: "GET", },
+  );
 }
-
 
 export async function deleteReview(id: string, token: string) {
   return fetchWrapper<APIResponseSingle<{}>>(
@@ -27,6 +30,6 @@ export async function deleteReview(id: string, token: string) {
       headers: {
         Authorization: `Bearer ${token}`,
       },
-    }
-  )
+    },
+  );
 }
